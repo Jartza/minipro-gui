9d28af3983410930c5d42aae640112398eee80df
vokamis <asimakov@twelvechairs-software.com>
2024-06-02 18:13:31 -0400

    Cleanup

--- src/Firmware.cpp
+++
@@ -1,1 +0,0 @@
-#include "Firmware.h"
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,5 @@
 #include <QApplication>
 #include "MainWindow.h"
-#include "Read.h"
-#include "Write.h"
-#include "Firmware.h"
 
 int main(int argc, char *argv[]) {
   QApplication a(argc, argv);
