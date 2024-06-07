a1f5ae1d042c371e7661da1715cac38d4cab88f4
vokamis <asimakov@twelvechairs-software.com>
2024-06-07 17:37:58 -0400

    Hex preview

--- src/MainWindow.cpp
+++ src/MainWindow.cpp
@@ -253,8 +253,8 @@ void MainWindow::update_firmware() {
 
 void MainWindow::build_default_hex_output(){
   hex_view->clear();
-  hex_view->appendPlainText("");
-  for (int n = 0; n <= 8; n++) {
+//  hex_view->appendPlainText("");
+  for (int n = 0; n < 8; n++) {
     hex_view->appendPlainText("-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --  ................");
   }
 }
