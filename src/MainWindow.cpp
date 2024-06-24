8459208c99f7532cb8f1a0a86f5c9123f0781c04
twelve-chairs <56702533+twelve-chairs@users.noreply.github.com>
2024-06-24 09:20:55 -0400

    Refactoring

--- src/MainWindow.cpp
+++ src/MainWindow.cpp
@@ -5,7 +5,7 @@ void MainWindow::initializer() {
   layout = new QGridLayout(window);
 
   window->setWindowTitle("minipro CLI not found!");
-  window->setMinimumSize(980, 480);
+  window->setMinimumSize(1100, 500);
 
   minipro_found = false;
   programmer_found = false;
